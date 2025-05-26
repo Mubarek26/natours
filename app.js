d5e5b22979e7e3a32b0777b113e1af0b1f1ccc47
Abdulkerim <kerimewa@gmail.com>
2025-05-26 18:26:32 +0300

    implement tours routes

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ import dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
-// Get __filename and __dirname equivalents
+import morgan from 'morgan';
 dotenv.config();
 
+// Get __filename and __dirname equivalents
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
+// 1. MIDDLEWARES
+
 app.use(express.json());
+app.use(morgan('dev')); // Logging middleware
 const PORT = process.env.PORT;
 const tours = JSON.parse(
   fs.readFileSync(
@@ -20,7 +24,8 @@ const tours = JSON.parse(
   )
 );
 
-app.get('/api/v1/tours', (req, res) => {
+// 2. ROUTE HANDLERS
+const getAllTours = (req, res) => {
   res.json({
     status: 'success',
     results: tours.length,
@@ -28,13 +33,97 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found',
+    });
+  }
+  res.json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
 
-})
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    path.join(__dirname, 'dev-data', 'data', 'tours-simple.json'),
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'error',
+          message: 'Error saving the tour',
+        });
+      }
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+const updateTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found',
+    });
+  }
+  res.json({
+    status: 'success',
+    data: {
+      tour: '<Updated Tour Here>',
+    },
+  });
+};
 
+const deleteTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
 
+// 3. ROUTES
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+// 4. START SERVER
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
