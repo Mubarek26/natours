62ecae6adde99c92d4983ff2472976b22d25464e
Abdulkerim <kerimewa@gmail.com>
2025-06-02 13:49:44 +0300

    configure env variables

--- app.js
+++ app.js
@@ -39,4 +39,6 @@ app.use(globalErrorHandler);
 // START SERVER
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  if(process.env.NODE_ENV === "development") console.log("In development mode");
+  else console.log("In production mode");
 });
