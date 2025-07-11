6c2dbd4984fe481f77f45ec7d2422aa57bb37902
Abdulkerim <kerimewa@gmail.com>
2025-07-11 10:26:37 +0300

    link uploaded image name with the user photo property

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -56,6 +56,7 @@ export const updateMe = catchAsync(async (req, res, next) => {
 
   // 2) Filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterBody(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
