a55b67eeb1c1453a526c77f0d5e33675e994b699
Abdulkerim <kerimewa@gmail.com>
2025-07-11 23:52:54 +0300

    add functionality to update a tour with cover image and tour image

--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -34,6 +34,8 @@ router
   .post(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
+    tourController.uploadTourImages,
+    tourController.resizeTourImages,
     tourController.createTour
   );
 router
@@ -42,6 +44,8 @@ router
   .patch(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
+    tourController.uploadTourImages,
+    tourController.resizeTourImages,
     tourController.updateTour
   )
   .delete(
